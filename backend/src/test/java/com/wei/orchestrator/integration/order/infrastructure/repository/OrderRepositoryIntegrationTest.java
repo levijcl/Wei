bee96aa68f4f4026c26eeef55210a8a854c6b32b
Levi Chiang <levilevi80@gmail.com>
2025-10-31 17:17:58 +0000

    [7_create_order_api] Fix transactional annotation

--- backend/src/test/java/com/wei/orchestrator/integration/order/infrastructure/repository/OrderRepositoryIntegrationTest.java
+++ backend/src/test/java/com/wei/orchestrator/integration/order/infrastructure/repository/OrderRepositoryIntegrationTest.java
@@ -15,15 +15,12 @@ import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
-import org.springframework.stereotype.Repository;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("test")
-@DataJpaTest(
-        includeFilters =
-                @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Repository.class))
+@DataJpaTest
+@Import(OrderRepositoryImpl.class)
 class OrderRepositoryIntegrationTest {
 
     @Autowired private OrderRepositoryImpl orderRepository;
